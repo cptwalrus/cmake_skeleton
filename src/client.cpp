8965ab5427e0ab497b57858feadd2d5a77d46e1a
CptWalrus <mrwalrus@mrwalrusdev.com>
2021-09-04 09:34:05 -0400

    Basic project init

---
+++ src/client.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <test.h>
+
+int main()
+{
+
+    std::cout << hello << "Client!" << std::endl;
+    return 0;
+}
---
+++ src/server.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <test.h>
+
+int main()
+{
+    std::cout << hello << "Server!" << std::endl;
+    return 0;
+}
